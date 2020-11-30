4eba21a4e94d9aeaf2d8fc33ca7c6458993f8a5a
07050586 <07050586@me.mcu.edu.tw>
2020-11-30 14:57:03 +0800

    modified:   mcsio.py

--- mcsio.py
+++ mcsio.py
@@ -1,1 +1,13 @@
-#Python
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24 GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    SwitchStatus = GPIO.input(24)
+    if( SwitchStatus == 0):
+        print('Button pressed')
+    else:
+        print('Button released')
+    payload = {"datapoints":[{"dataChnId":"Hum","values":{"value":humidity}}
+                             ,{"dataChnId":"Temp","values":temperature}},{"dataChnId":"SwitchStatus"
+                             ,"values":{"values":SwitchStatus}}]}
